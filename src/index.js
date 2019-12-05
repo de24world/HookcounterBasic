dff3c29ed812c61eb2d866bd942b1e67c36574da
de24world <30653357+de24world@users.noreply.github.com>
2019-12-05 11:48:14 +0100

    update validator value
    willupdate and validator

--- src/index.js
+++ src/index.js
@@ -3,16 +3,43 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = event => {
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
 const App = () => {
   const [item, setItem] = useState(1);
   const incrementItem = () => setItem(item + 1);
   const decrementItem = () => setItem(item - 1);
+
+  const maxLen = value => value.length <= 10;
+  const name = useInput("Mr.", maxLen);
+  const email = useInput("@");
   return (
     <div className="App">
       <h1>Hello {item}</h1>
       <h2>Start editing to see some magic happen!</h2>
       <button onClick={incrementItem}>Increment</button>
-      <button onClick={decrementItem}>Decrement</button>
+      <button onClick={decrementItem}>Decrement</button> <br />
+      <input
+        placeholder="Name"
+        {...name}
+        // =value={name.value}
+      />
+      <input placeholder="Email" {...email} />
     </div>
   );
 };
